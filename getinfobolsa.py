6c20585ef6e83efc01b79f2003bd3b0d417db7b2
NCordon <nacho.cordon.castillo@gmail.com>
2016-06-04 02:17:10 +0200

    Scrapeo de variaciones periódicas hecho

--- getinfobolsa.py
+++ getinfobolsa.py
@@ -7,14 +7,13 @@ import requests
 from lxml import html
 import json
 import re
-
+from diccionario import empresas
 
 def to_float(s):
-    return s.replace(".","").replace(",",".")
+    return float(s.replace(".","").replace(",",".").replace("%",""))
 
-
-data = {'name':[], 'price':[], 'var':[], 'capitalizacion:'[],
-        'per':[], 'rdp':[]}
+# precio   var_ayer   capitalizacion   var_mes   var_tri   var_sem   var_año
+data = {}
 
 root = "http://www.bolsamadrid.es"
 
@@ -32,16 +31,13 @@ for r in rows:
     name = cols[0].xpath('.//a//text()')[0]
     price = to_float(cols[1].text)
     var = to_float(cols[2].text)
-    data['name'] += [name]
-    data['price'] += [price]
-    data['var'] += [var]
+
 
     link_deep = root + cols[0].xpath('.//a//@href')[0]
     link_page = html.fromstring(requests.get(link_deep).content)
     capitalizacion = link_page.xpath('//table[@id="ctl00_Contenido_tblCapital"]//tr')[1].\
                      getchildren()[1].text
     capitalizacion = to_float(capitalizacion)
-    data['capitalizacion'] += [capitalizacion]
 
 
     # Histórico de un año, por si es necesario
@@ -49,3 +45,29 @@ for r in rows:
     ancient = root +\
               re.search('url: "/aspx/comun/consultaPrecios.*"', script_grafico).\
               group(0).rsplit()[-1].replace("\"","")
+
+
+    data[name] = []
+    data[name] += [price]
+    data[name] += [var]
+    data[name] += [capitalizacion]
+
+
+
+periodos = ["mes", "trimestre", "semana", "interanual"]
+
+for periodo in periodos:
+    query = "http://www.invertia.com/mercados/bolsa/indices/ibex-35/acciones-"\
+            + periodo + "-ib011ibex35"
+    page = html.fromstring(requests.get(query).content)
+    rows = page.xpath('//table[@title="Acciones"]//tr')
+    # Le quitamos la cabecera
+    rows = rows[1:]
+
+    for r in rows:
+        cols = r.getchildren()
+
+        name = cols[0].xpath(".//a//@title")[0]
+        if name in empresas.keys():
+            variacion_periodo = to_float(cols[3].text)
+            data[empresas[name]] += [variacion_periodo]
